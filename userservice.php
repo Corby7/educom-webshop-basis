a840276cd5e35c7bcafcc3aba130e8464395e1b5
Corby7 <corbijn.bulsink@hotmail.com>
2023-09-19 15:34:27 +0200

    commented out unused code for now #10

--- userservice.php
+++ userservice.php
@@ -1,7 +1,5 @@
 <?php
 
-require('filerepository.php');
-
 function getSalutation($gender) {
     switch ($gender) {
         case 'male':
@@ -11,21 +9,6 @@ function getSalutation($gender) {
         default:
             return;
     }
-}
-
-//if email is known already, show error
-function handleKnownEmail($inputdata) {
-    $inputdata['emailknownErr'] = "E-mailadres is reeds bekend";
-    showRegisterForm($inputdata);
-}
-
-//if email is unknown, save new userdata, send to login
-function handleUnknownEmail($email, $name, $pass) {
-    echo "email is not found";
-    saveUser($email, $name, $pass);
-    //not sure if i can use header?
-    header('Location: index.php?page=login');
-    exit; //exit to prevent further execution
 }
 
 define("RESULT_OK", 0);
@@ -46,23 +29,20 @@ function authenticateUser($email, $pass) {
     return ['result' => RESULT_OK, 'user' => $user]; //email matches password
 }
 
-function handleAuthentication($result, $inputdata) {
-    switch ($result['result']) {
-        case RESULT_UNKNOWN_USER;
-            $inputdata['emailunknownErr'] = "E-mailadres is onbekend";
-            break;
-        case RESULT_WRONG_PASSWORD;
-            $inputdata['wrongpassErr'] = "Wachtwoord is onjuist";
-            break;
-        case RESULT_OK;
-            $user = $result['user'];
-            echo 'Authentication successful: ' . $user['name'] . ';';
-            loginUser($user['name']);
-            
-    }
-
-    showLoginForm($inputdata);
-}
+// function handleAuthentication($result, $inputdata) {
+//     switch ($result['result']) {
+//         case RESULT_UNKNOWN_USER;
+//             $inputdata['emailunknownErr'] = "E-mailadres is onbekend";
+//             break;
+//         case RESULT_WRONG_PASSWORD;
+//             $inputdata['wrongpassErr'] = "Wachtwoord is onjuist";
+//             break;
+//         case RESULT_OK;
+//             $user = $result['user'];
+//             echo 'Authentication successful: ' . $user['name'] . ';';
+//             loginUser($user['name']);
+//     }
+// }
 
 function doesEmailExist($email) {
     $user = findUserByEmail($email);
